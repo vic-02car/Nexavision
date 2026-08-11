5b6bcc4c43d2b55ae962a907e6ab88a981771ea9
vic-02car <vcardenasb@icttalagante.cl>
2026-08-11 14:59:39 -0400

    Update qr.py

--- qr.py
+++ qr.py
@@ -1,9 +1,24 @@
 import qrcode
-input='import qrcode'
-input='https://vic-02car.github.io/Nexavision/' #esto es lo que se guardara en el QRCode
-qr = qrcode.QRCode(version=1, box_size=10, border=5)
-qr.add_data(input)
+from qrcode.image.pil import PilImage
+
+url = "https://vic-02car.github.io/Nexavision/"
+
+qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_H,
+    box_size=10,
+    border=4
+)
+
+qr.add_data(url)
 qr.make(fit=True)
-img=qr.make_image(fill='Black',back_color='white') # señalamos que el relleno sea negro 
-#y el fondo blanco
-img.save('qrnexa.png') #se genera este archivo QRCode
+
+img = qr.make_image(
+    image_factory=PilImage,
+    fill_color="#00C853",
+    back_color="#0A2540"
+)
+
+img.save("qrnexa.png")
+
+print("QR creado correctamente")
